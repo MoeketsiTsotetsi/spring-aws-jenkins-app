eaf0aac0b1223413b8b1ce0fff81003d31bf3a0f
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2022-10-02 23:18:59 +0200

    test fix

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,10 +1,14 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+
 public class IndexController {
+	
+	@RequestMapping(value = "")
 	public ModelAndView showIndexPage() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("index.jsp");
