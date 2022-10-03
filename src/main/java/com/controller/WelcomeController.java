d6bb0ff60dff6ab5b8e01f71c3ed52c1a3a1c00e
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2022-10-03 17:55:44 +0200

    added thymeleaf v 2

--- src/main/java/com/controller/WelcomeController.java
+++ src/main/java/com/controller/WelcomeController.java
@@ -1,14 +1,15 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WelcomeController {
 
 	
 	@GetMapping(value = "/")
 	public String welcome() {
-		return "Welcome ,to test the web app enter /tools ";
+		return "index";
 	}
 }
