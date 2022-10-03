c70363632181ee6bb098e30b69ec9fca1171ad1c
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2022-10-03 17:43:07 +0200

    added thymeleaf

--- src/main/java/com/SBAJApplication.java
+++ src/main/java/com/SBAJApplication.java
@@ -2,7 +2,6 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "com")
 public class SBAJApplication {
