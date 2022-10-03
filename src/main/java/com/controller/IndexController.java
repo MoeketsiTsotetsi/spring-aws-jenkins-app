29178150e0d81271c217e4b07b2948170f2ad511
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2022-10-03 12:34:43 +0200

    changed port no to 8282

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,14 +1,13 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	
 	public String showIndexPage() {
 		return "index";
